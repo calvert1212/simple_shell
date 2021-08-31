c32626242d7ab65ba1ce5a4e20c445d8413bee51
RockoGilbert <evolve2122@gmail.com>
2021-08-31 13:20:47 -0500

    Stops exit on Ctrl C

--- _stopcc.c
+++ _stopcc.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 
 /**
- * _stopcc - Ctrl C stops exit
- * @signal: Signal number
+ * _stopcc - Stops exit on Ctrl C
+ * @sig: Signal number
  * Description: Prevents Ctrl C exit
  * Return: None
  */
-
-void _stopcc(int signal)
+void _stopcc(int sig)
 {
 	(void) sig;
 	write(STDOUT_FILENO, "\n$ ", 3);
