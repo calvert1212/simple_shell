3337eec053cb3399576d7907d8fa64085813cb93
calvert1212 <calvert1212@gmail.com>
2021-11-30 20:47:24 +0000

    Fixed prompt to use write

--- prompt.c
+++ prompt.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "shell.h"
 
+/**
+ * print_prompt1 - writes prompt
+ * Return: void
+ */
+
 void print_prompt1(void)
 {
-    fprintf(stderr, "$ ");
+  write(STDOUT_FILENO, "$ ", 2);
 }
