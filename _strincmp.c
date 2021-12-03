4baba087577f69bbbb5fbef2c2aa97f9ae0f5747
CalicoWhoMan <mikeyshrier@gmail.com>
2021-12-02 16:27:26 -0800

    fixed betty issues

--- _strincmp.c
+++ _strincmp.c
@@ -10,9 +10,9 @@
 
 int _strincmp(char *s1, char *s2, int x)
 {
-  int i = 0;
+	int i = 0;
 
-  while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < x - 1)
-    i++;
-  return (s1[i] - s2[i]);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i] && i < x - 1)
+	i++;
+	return (s1[i] - s2[i]);
 }
